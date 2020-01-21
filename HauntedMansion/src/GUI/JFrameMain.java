342a98cce0987af677275c31dcf7aa6d402e4fef
Vmvs007 <vmvs007@gmail.com>
2020-01-21 11:26:02 +0000

    GUI Creation

--- HauntedMansion/src/GUI/JFrameMain.java
+++ HauntedMansion/src/GUI/JFrameMain.java
@@ -5,6 +5,16 @@
  */
 package GUI;
 
+import static java.awt.Color.black;
+import static java.awt.Color.lightGray;
+import static java.awt.Color.red;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Panel;
+import java.awt.Toolkit;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
 /**
  *
  * @author vmvs0
@@ -27,23 +37,48 @@ public class JFrameMain extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel1 = new javax.swing.JPanel();
+        panelBackground = new javax.swing.JPanel();
+        labelGameName = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Haunted Mansion");
         setPreferredSize(new java.awt.Dimension(1080, 720));
 
-        jPanel1.setBackground(new java.awt.Color(153, 153, 153));
+        panelBackground.setBackground(new java.awt.Color(153, 153, 153));
+        panelBackground.addComponentListener(new java.awt.event.ComponentAdapter() {
+            public void componentShown(java.awt.event.ComponentEvent evt) {
+                panelBackgroundComponentShown(evt);
+            }
+        });
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 720, Short.MAX_VALUE)
+        labelGameName.setFont(new java.awt.Font("Tahoma", 0, 36)); // NOI18N
+        labelGameName.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        labelGameName.setText("Haunted Mansion Game");
+        labelGameName.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        labelGameName.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                labelGameNameMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                labelGameNameMouseExited(evt);
+            }
+        });
+
+        javax.swing.GroupLayout panelBackgroundLayout = new javax.swing.GroupLayout(panelBackground);
+        panelBackground.setLayout(panelBackgroundLayout);
+        panelBackgroundLayout.setHorizontalGroup(
+            panelBackgroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelBackgroundLayout.createSequentialGroup()
+                .addGap(339, 339, 339)
+                .addComponent(labelGameName)
+                .addContainerGap(338, Short.MAX_VALUE))
         );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 694, Short.MAX_VALUE)
+        panelBackgroundLayout.setVerticalGroup(
+            panelBackgroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelBackgroundLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(labelGameName)
+                .addContainerGap(637, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -51,21 +86,37 @@ public class JFrameMain extends javax.swing.JFrame {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(348, Short.MAX_VALUE)
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap()
+                .addComponent(panelBackground, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(panelBackground, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addContainerGap())
         );
 
         pack();
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
+
+    private void labelGameNameMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_labelGameNameMouseEntered
+        //Changes label color to Red when Mouse Enters
+        this.labelGameName.setBackground(red); 
+        this.labelGameName.setForeground(red);
+    }//GEN-LAST:event_labelGameNameMouseEntered
+
+    private void labelGameNameMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_labelGameNameMouseExited
+        //Changes label color back to Black when Mouse Exits
+        this.labelGameName.setBackground(black); 
+        this.labelGameName.setForeground(black);
+    }//GEN-LAST:event_labelGameNameMouseExited
+
+    private void panelBackgroundComponentShown(java.awt.event.ComponentEvent evt) {//GEN-FIRST:event_panelBackgroundComponentShown
+        
+    }//GEN-LAST:event_panelBackgroundComponentShown
 
     /**
      * @param args the command line arguments
@@ -99,6 +150,10 @@ public class JFrameMain extends javax.swing.JFrame {
         }
         //</editor-fold>
 
+        ImageIcon icon = new ImageIcon("HauntedImg.jpg"); 
+        JLabel thumb = new JLabel();
+        thumb.setIcon(icon);
+        
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
@@ -108,6 +163,7 @@ public class JFrameMain extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JPanel jPanel1;
+    private javax.swing.JLabel labelGameName;
+    private javax.swing.JPanel panelBackground;
     // End of variables declaration//GEN-END:variables
 }
