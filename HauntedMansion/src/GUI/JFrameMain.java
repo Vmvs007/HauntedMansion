e24b7ac2e56d3c240af0f8060ed3a8b27a1113b1
Vmvs007 <vmvs007@gmail.com>
2020-01-23 20:32:31 +0000

    GUI Update

--- HauntedMansion/src/GUI/JFrameMain.java
+++ HauntedMansion/src/GUI/JFrameMain.java
@@ -25,6 +25,9 @@ import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.imageio.ImageIO;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JFrame;
@@ -56,10 +59,20 @@ public class JFrameMain extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jRadioButton1 = new javax.swing.JRadioButton();
         panelBackground = new javax.swing.JPanel();
+        jLayeredPane1 = new javax.swing.JLayeredPane();
+        mainPanel = new javax.swing.JPanel();
+        playButton = new javax.swing.JButton();
+        tutorialButton = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        creditsButton = new javax.swing.JButton();
+        exitButton = new javax.swing.JButton();
+        titleButton = new javax.swing.JButton();
+        mansionPanel = new javax.swing.JPanel();
         labelBack = new javax.swing.JLabel();
-        jPanel1 = new javax.swing.JPanel();
-        labelGameName = new javax.swing.JLabel();
+
+        jRadioButton1.setText("jRadioButton1");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Haunted Mansion");
@@ -76,88 +89,191 @@ public class JFrameMain extends javax.swing.JFrame {
             }
         });
 
-        ImageIcon icon = new ImageIcon("HauntedImg02.png");
-        labelBack.setIcon(icon);
+        jLayeredPane1.setBackground(new java.awt.Color(255, 153, 153));
+        jLayeredPane1.setForeground(new java.awt.Color(255, 102, 102));
 
-        jPanel1.setBackground(new java.awt.Color(0, 0, 0));
-
-        labelGameName.setFont(new java.awt.Font("Tahoma", 0, 36)); // NOI18N
-        labelGameName.setForeground(new java.awt.Color(255, 255, 255));
-        labelGameName.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        labelGameName.setText("Haunted Mansion Game");
-        labelGameName.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        labelGameName.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseEntered(java.awt.event.MouseEvent evt) {
-                labelGameNameMouseEntered(evt);
-            }
-            public void mouseExited(java.awt.event.MouseEvent evt) {
-                labelGameNameMouseExited(evt);
+        playButton.setBackground(new java.awt.Color(0, 0, 0));
+        playButton.setFont(new java.awt.Font("October Crow", 1, 36)); // NOI18N
+        playButton.setForeground(new java.awt.Color(255, 0, 0));
+        playButton.setText("PLAY");
+        playButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                playButtonActionPerformed(evt);
             }
         });
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(38, Short.MAX_VALUE)
-                .addComponent(labelGameName)
-                .addGap(29, 29, 29))
+        tutorialButton.setBackground(new java.awt.Color(0, 0, 0));
+        tutorialButton.setFont(new java.awt.Font("October Crow", 0, 36)); // NOI18N
+        tutorialButton.setForeground(new java.awt.Color(255, 0, 0));
+        tutorialButton.setText("HOW TO PLAY");
+        tutorialButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                tutorialButtonActionPerformed(evt);
+            }
+        });
+
+        jButton2.setBackground(new java.awt.Color(0, 0, 0));
+        jButton2.setFont(new java.awt.Font("October Crow", 0, 36)); // NOI18N
+        jButton2.setForeground(new java.awt.Color(255, 0, 0));
+        jButton2.setText("456");
+
+        creditsButton.setBackground(new java.awt.Color(0, 0, 0));
+        creditsButton.setFont(new java.awt.Font("October Crow", 0, 36)); // NOI18N
+        creditsButton.setForeground(new java.awt.Color(255, 0, 0));
+        creditsButton.setText("CREDITS");
+        creditsButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                creditsButtonActionPerformed(evt);
+            }
+        });
+
+        exitButton.setBackground(new java.awt.Color(0, 0, 0));
+        exitButton.setFont(new java.awt.Font("October Crow", 0, 36)); // NOI18N
+        exitButton.setForeground(new java.awt.Color(255, 0, 0));
+        exitButton.setText("EXIT");
+        exitButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                exitButtonActionPerformed(evt);
+            }
+        });
+
+        titleButton.setBackground(new java.awt.Color(0, 0, 0));
+        titleButton.setFont(new java.awt.Font("October Crow", 1, 58)); // NOI18N
+        titleButton.setForeground(new java.awt.Color(255, 0, 51));
+        titleButton.setText("HAUNTED MANSION GAME");
+        titleButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                titleButtonActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout mainPanelLayout = new javax.swing.GroupLayout(mainPanel);
+        mainPanel.setLayout(mainPanelLayout);
+        mainPanelLayout.setHorizontalGroup(
+            mainPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, mainPanelLayout.createSequentialGroup()
+                .addContainerGap(262, Short.MAX_VALUE)
+                .addComponent(titleButton, javax.swing.GroupLayout.PREFERRED_SIZE, 682, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(257, 257, 257))
+            .addGroup(mainPanelLayout.createSequentialGroup()
+                .addGap(396, 396, 396)
+                .addGroup(mainPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(playButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(tutorialButton, javax.swing.GroupLayout.DEFAULT_SIZE, 400, Short.MAX_VALUE)
+                    .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(creditsButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(exitButton, javax.swing.GroupLayout.PREFERRED_SIZE, 400, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(405, Short.MAX_VALUE))
         );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(labelGameName)
+        mainPanelLayout.setVerticalGroup(
+            mainPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(mainPanelLayout.createSequentialGroup()
+                .addGap(34, 34, 34)
+                .addComponent(titleButton, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(91, 91, 91)
+                .addComponent(playButton)
+                .addGap(18, 18, 18)
+                .addComponent(tutorialButton, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(creditsButton, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(exitButton, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(85, Short.MAX_VALUE))
+        );
+
+        mansionPanel.setBackground(new java.awt.Color(153, 102, 255));
+
+        ImageIcon icon = new ImageIcon("HauntedImg02.png");
+        labelBack.setIcon(icon);
+
+        javax.swing.GroupLayout mansionPanelLayout = new javax.swing.GroupLayout(mansionPanel);
+        mansionPanel.setLayout(mansionPanelLayout);
+        mansionPanelLayout.setHorizontalGroup(
+            mansionPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(labelBack, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 1200, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+        mansionPanelLayout.setVerticalGroup(
+            mansionPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(mansionPanelLayout.createSequentialGroup()
+                .addComponent(labelBack, javax.swing.GroupLayout.DEFAULT_SIZE, 635, Short.MAX_VALUE)
                 .addContainerGap())
+        );
+
+        jLayeredPane1.setLayer(mainPanel, javax.swing.JLayeredPane.DEFAULT_LAYER);
+        jLayeredPane1.setLayer(mansionPanel, javax.swing.JLayeredPane.DEFAULT_LAYER);
+
+        javax.swing.GroupLayout jLayeredPane1Layout = new javax.swing.GroupLayout(jLayeredPane1);
+        jLayeredPane1.setLayout(jLayeredPane1Layout);
+        jLayeredPane1Layout.setHorizontalGroup(
+            jLayeredPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(mansionPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(jLayeredPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addComponent(mainPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        jLayeredPane1Layout.setVerticalGroup(
+            jLayeredPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jLayeredPane1Layout.createSequentialGroup()
+                .addComponent(mansionPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 27, Short.MAX_VALUE))
+            .addGroup(jLayeredPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(jLayeredPane1Layout.createSequentialGroup()
+                    .addComponent(mainPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGap(0, 22, Short.MAX_VALUE)))
         );
 
         javax.swing.GroupLayout panelBackgroundLayout = new javax.swing.GroupLayout(panelBackground);
         panelBackground.setLayout(panelBackgroundLayout);
         panelBackgroundLayout.setHorizontalGroup(
             panelBackgroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(labelBack, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelBackgroundLayout.createSequentialGroup()
-                .addContainerGap(365, Short.MAX_VALUE)
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(389, 389, 389))
+            .addGroup(panelBackgroundLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLayeredPane1)
+                .addContainerGap())
         );
         panelBackgroundLayout.setVerticalGroup(
             panelBackgroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(panelBackgroundLayout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(labelBack, javax.swing.GroupLayout.PREFERRED_SIZE, 664, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addComponent(jLayeredPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(panelBackground, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(panelBackground, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(panelBackground, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(panelBackground, javax.swing.GroupLayout.PREFERRED_SIZE, 675, Short.MAX_VALUE)
         );
 
         pack();
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void labelGameNameMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_labelGameNameMouseEntered
-        //Changes label color to Red when Mouse Enters
-        this.labelGameName.setBackground(red);
-        this.labelGameName.setForeground(red);
-    }//GEN-LAST:event_labelGameNameMouseEntered
-
-    private void labelGameNameMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_labelGameNameMouseExited
-        //Changes label color back to Black when Mouse Exits
-        this.labelGameName.setBackground(white);
-        this.labelGameName.setForeground(white);
-    }//GEN-LAST:event_labelGameNameMouseExited
-
+   /**
+    * 
+    * @param soundName 
+    */
+   public void playSound(String soundName){
+   try 
+   {
+    AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(soundName).getAbsoluteFile());
+    Clip clip = AudioSystem.getClip();
+    clip.open(audioInputStream);
+    clip.start();
+   }
+   catch(Exception ex)
+   {
+     System.out.println("Error with playing sound.");
+     ex.printStackTrace( );
+   }
+ }
+   
     private void panelBackgroundComponentShown(java.awt.event.ComponentEvent evt) {//GEN-FIRST:event_panelBackgroundComponentShown
 
     }//GEN-LAST:event_panelBackgroundComponentShown
@@ -166,12 +282,39 @@ public class JFrameMain extends javax.swing.JFrame {
 
     }//GEN-LAST:event_panelBackgroundComponentAdded
 
+    private void playButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_playButtonActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_playButtonActionPerformed
+
+    private void tutorialButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_tutorialButtonActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_tutorialButtonActionPerformed
+
+    private void exitButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_exitButtonActionPerformed
+        //Quando o exitButton é premido, fecha a aplicação
+        System.exit(0);
+    }//GEN-LAST:event_exitButtonActionPerformed
+
+    private void creditsButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_creditsButtonActionPerformed
+        //Quando o creditsButton é premido, abre a janela dos créditos
+           java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new JFrameCredits().setVisible(true);
+            }
+        });
+    }//GEN-LAST:event_creditsButtonActionPerformed
+
+    private void titleButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_titleButtonActionPerformed
+        // TODO add your handling code here:
+        playSound("SwampSound.wav");
+    }//GEN-LAST:event_titleButtonActionPerformed
+
     /**
      * @param args the command line arguments
      */
     public static void main(String args[]) {
 
-        //Script starts here
+        //Script começa aqui
         System.out.println("_______________ Haunted Mansion Starts _______________");
 
         /* Set the Nimbus look and feel */
@@ -206,9 +349,17 @@ public class JFrameMain extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JPanel jPanel1;
+    private javax.swing.JButton creditsButton;
+    private javax.swing.JButton exitButton;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JLayeredPane jLayeredPane1;
+    private javax.swing.JRadioButton jRadioButton1;
     private javax.swing.JLabel labelBack;
-    private javax.swing.JLabel labelGameName;
+    private javax.swing.JPanel mainPanel;
+    private javax.swing.JPanel mansionPanel;
     private javax.swing.JPanel panelBackground;
+    private javax.swing.JButton playButton;
+    private javax.swing.JButton titleButton;
+    private javax.swing.JButton tutorialButton;
     // End of variables declaration//GEN-END:variables
 }
